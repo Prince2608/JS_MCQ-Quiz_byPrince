27afd54f2c37d9ef0704d571809918e475d55a84
Prince2608 <priman2611@gmail.com>
2021-11-28 17:39:01 +0530

    done

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: " Which type of JavaScript language is",
+    answer: "Object-Based",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Assembly-language",
+      "High-level"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which one of the following also known as Conditional Expression:",
+    answer: "immediate if",
+    options: [
+      "Alternative to if-else",
+      "Switch statement",
+      "If-then-else statement",
+      "immediate if"
+    ]
+  },
+  {
+    numb: 3,
+    question: "In JavaScript, what is a block of statement?",
+    answer: "block that combines a number of statements into a single compound statement",
+    options: [
+      "Conditional block",
+      "block that combines a number of statements into a single compound statement",
+      "both conditional block and a single statement",
+      "block that contains a single statement"
+    ]
+  },
+  {
+    numb: 4,
+    question: "When interpreter encounters an empty statements, what it will do:?",
+    answer: "Ignores the statements",
+    options: [
+      "Shows a warning",
+      "Prompts to complete the statement",
+      "Throws an error",
+      "Ignores the statements" 
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which of the following variables takes precedence over the others if the names are the same?",
+    answer: "The local element",
+    options: [
+      "Global variable",
+      "The local element",
+      "The two of the above",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 6,
+    question: " Which one of the following is the correct way for calling the JavaScript code?",
+    answer: "Function/Method",
+    options: [
+      "Preprocessor",
+      "Triggering Event",
+      "RMI",
+      "Function/Method"
+    ]
+  },
+  {
+    numb: 7,
+    question: " Which of the following type of a variable is volatile?",
+    answer: "Mutable variable",
+    options: [
+      "Mutable variable",
+      "Dynamic variable",
+      "Volatile variable",
+      "Immutable variable"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following option is used as hexadecimal literal beginning?",
+    answer: "Both 0x and 0X",
+    options: [
+      "00",
+      "0x",
+      "0X",
+      "Both 0x and 0X"
+    ]
+  },
+  {
+    numb: 9,
+    question: " In the JavaScript, which one of the following is not considered as an error?",
+    answer: "Division by zero",
+    options: [
+      "Syntax error",
+      "Missing of semicolons",
+      "Division by zero",
+      "Missing of Bracket"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following number object function returns the value of the number?",
+    answer: "valueOf()",
+    options: [
+      "toString()",
+      "valueOf()",
+      "toLocaleString()",
+      "toPrecision()"
+    ]
+  },
+
+];
